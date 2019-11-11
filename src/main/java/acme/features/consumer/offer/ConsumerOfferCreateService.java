0bad7d85598166b165d35e5379602a0d6b5dbb98
GGarridoDP14 <guigarjim@alum.us.es>
2019-11-12 00:28:50 +0100

    Los usuarios consumidores ya pueden crear ofertas El checkbox del formulario de crear una peticion por un proveedor debería funcionar ahora correctamente

--- src/main/java/acme/features/consumer/offer/ConsumerOfferCreateService.java
+++ src/main/java/acme/features/consumer/offer/ConsumerOfferCreateService.java
@@ -1,38 +1,38 @@
 
-package acme.features.provider.request;
+package acme.features.consumer.offer;
 
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import acme.entities.requests.Request;
-import acme.entities.roles.Provider;
+import acme.entities.offers.Offer;
+import acme.entities.roles.Consumer;
 import acme.framework.components.Errors;
 import acme.framework.components.HttpMethod;
 import acme.framework.components.Model;
 import acme.framework.services.AbstractCreateService;
 
 @Service
-public class ProviderRequestCreateService implements AbstractCreateService<Provider, Request> {
+public class ConsumerOfferCreateService implements AbstractCreateService<Consumer, Offer> {
 
 	// Internal state ---------------------------------------------------------
 
 	@Autowired
-	ProviderRequestRepository repository;
+	ConsumerOfferRepository repository;
 
 
-	// AbstractCreateService<Provider, Request> interface ---------------
+	// AbstractCreateService<Consumer, Offer> interface ---------------
 
 	@Override
-	public boolean authorise(final acme.framework.components.Request<Request> request) {
+	public boolean authorise(final acme.framework.components.Request<Offer> request) {
 		assert request != null;
 
 		return true;
 	}
 
 	@Override
-	public void bind(final acme.framework.components.Request<Request> request, final Request entity, final Errors errors) {
+	public void bind(final acme.framework.components.Request<Offer> request, final Offer entity, final Errors errors) {
 		assert request != null;
 		assert entity != null;
 		assert errors != null;
@@ -42,12 +42,12 @@ public class ProviderRequestCreateService implements AbstractCreateService<Provi
 	}
 
 	@Override
-	public void unbind(final acme.framework.components.Request<Request> request, final Request entity, final Model model) {
+	public void unbind(final acme.framework.components.Request<Offer> request, final Offer entity, final Model model) {
 		assert request != null;
 		assert entity != null;
 		assert model != null;
 
-		request.unbind(entity, model, "title", "deadline", "text", "reward", "ticker");
+		request.unbind(entity, model, "title", "deadline", "text", "minMoney", "maxMoney", "ticker");
 
 		if (request.isMethod(HttpMethod.GET)) {
 			model.setAttribute("accept", "false");
@@ -57,29 +57,29 @@ public class ProviderRequestCreateService implements AbstractCreateService<Provi
 	}
 
 	@Override
-	public Request instantiate(final acme.framework.components.Request<Request> request) {
-		Request result;
+	public Offer instantiate(final acme.framework.components.Request<Offer> request) {
+		Offer result;
 
-		result = new Request();
+		result = new Offer();
 
 		return result;
 	}
 
 	@Override
-	public void validate(final acme.framework.components.Request<Request> request, final Request entity, final Errors errors) {
+	public void validate(final acme.framework.components.Request<Offer> request, final Offer entity, final Errors errors) {
 		assert request != null;
 		assert entity != null;
 		assert errors != null;
 
-		Boolean isAccepted;
+		boolean isAccepted;
 
 		isAccepted = request.getModel().getBoolean("accept");
-		errors.state(request, isAccepted, "accept", "provider.request.error.must-accept");
+		errors.state(request, isAccepted, "accept", "consumer.offer.error.must-accept");
 
 	}
 
 	@Override
-	public void create(final acme.framework.components.Request<Request> request, final Request entity) {
+	public void create(final acme.framework.components.Request<Offer> request, final Offer entity) {
 		Date moment;
 
 		moment = new Date(System.currentTimeMillis() - 1);
